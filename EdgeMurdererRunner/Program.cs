83f350f6e96a094b096d1bc73b41be5fff82e85c
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2021-11-13 13:42:06 -0700

    Add grim reaper
    Allows redirecting the opening of a file, or link to a different, less bad browser.
    Did cleanup on unused references

--- EdgeMurdererRunner/Program.cs
+++ EdgeMurdererRunner/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Management;
+using System.Text.RegularExpressions;
 
 namespace EdgeMurdererRunner
 {
@@ -11,13 +13,18 @@ namespace EdgeMurdererRunner
 		//private static List<string> blacklistedPrograms = new List<string>(new[] { "msedge", "microsoft edge", "notepad", "microsoftedgeupdate" });
 		private static List<string> blacklistedPrograms = new List<string>();
 
+		private static long lastFileOpenTime = 0;
+		private static string lastFileOpenArgs = "";
+
+		private const long fileOpenDelay = 5000;
+
 		public static void Main(string[] args)
 		{
 			LoadSettings();
 
 			WqlEventQuery query =
 				new WqlEventQuery("__InstanceCreationEvent",
-					new TimeSpan(0, 0, 2),
+					new TimeSpan(0, 0, 0, 0, 250),
 					"TargetInstance isa \"Win32_Process\"");
 
 			ManagementEventWatcher watcher =
@@ -37,6 +44,45 @@ namespace EdgeMurdererRunner
 					{
 						try
 						{
+							if (pName == "msedge.exe")
+							{
+								var argz = Regex.Replace(GetCommandLine(p), "^\\\".*msedge\\.exe\\\" ", "");
+								Uri uriResult;
+								if (Uri.TryCreate(argz, UriKind.Absolute, out uriResult)
+								    && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+								{
+									if (CurrentTime() - lastFileOpenTime > fileOpenDelay || lastFileOpenArgs != argz)
+									{
+										Process.Start("chrome", argz);
+										Console.WriteLine("\tRespawning in chrome: " + argz);
+										lastFileOpenTime = CurrentTime();
+										lastFileOpenArgs = argz;
+									}
+									else
+									{
+										Console.WriteLine("\tDeclining to respawn in chrome: " + argz + " (" + CurrentTime() + ", " + lastFileOpenTime + ") (" + lastFileOpenArgs + ")");
+									}
+								}
+								else
+								{
+									argz = Regex.Replace(argz, "^--single-argument ", "");
+									if (File.Exists(argz))
+									{
+										if (CurrentTime() - lastFileOpenTime > fileOpenDelay || lastFileOpenArgs != argz)
+										{
+											Process.Start("chrome", argz);
+											Console.WriteLine("\tRespawning in chrome: " + argz);
+											lastFileOpenTime = CurrentTime();
+											lastFileOpenArgs = argz;
+										}
+										else
+										{
+											Console.WriteLine("\tDeclining to respawn in chrome: " + argz + " (" + CurrentTime() + ", " + lastFileOpenTime + ") (" + lastFileOpenArgs + ")");
+										}
+									}
+								}
+							}
+
 							p.Kill();
 							Console.WriteLine("\tKilled " + p.ProcessName + " (ID: " + p.Id + ")");
 						}
@@ -58,14 +104,30 @@ namespace EdgeMurdererRunner
 			if (!File.Exists("blacklisted_programs.ini"))
 			{
 				blacklistedPrograms.Add("msedge");
-			}else 
+			}
+			else
 				foreach (string line in File.ReadLines("blacklisted_programs.ini"))
 					if (line != "")
 						blacklistedPrograms.Add(line);
+
 			Console.WriteLine("Blacklisted programs: ");
 			foreach (string line in blacklistedPrograms)
 				Console.WriteLine(line + ".exe");
 			Console.WriteLine("======================");
+		}
+
+		private static string GetCommandLine(Process process)
+		{
+			using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+			using (ManagementObjectCollection objects = searcher.Get())
+			{
+				return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+			}
+		}
+
+		private static long CurrentTime()
+		{
+			return DateTimeOffset.Now.ToUnixTimeMilliseconds();
 		}
 	}
 }
--- readme.md
+++ readme.md
@@ -5,7 +5,8 @@ This does exactly what you think it does, and more! By default it will murder on
 
 ### YT vid
 Check out a short demo [here](https://youtu.be/WrKcuZmqNyU)
+A second video is [here](https://youtu.be/jNNuB4FBpDM)
 
 ### Todo (if people like this)
--[] Have this automatically run on system boot
+- [] Have this automatically run on system boot
 - I take suggestions too!
